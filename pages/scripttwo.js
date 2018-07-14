749770594946431d8a8c6264d4309e5d9964b371
Dean Moore <deanmoore009@gmail.com>
2018-07-15 00:16:34 +0100

    JQuery
    More fooling around with JQuery testing fade in's

--- pages/scripttwo.js
+++ pages/scripttwo.js
@@ -1,3 +1,26 @@
-$("home").click(function(){
-    $("navbar").animate({right: '250px'});
+$(document).ready(fadeIn);
+    
+
+function fadeIn(){
+    $(".navbar").hide(0).delay(500).fadeIn(900);
+}
+
+//below doesn't work as intended
+$(document).click(function(event) {
+    var text = $(event.target).text();
 });
+
+
+function clicked(){
+    text.css("fontSize", 300);
+}
+
+if (location.pathname == '/indexto.html'){
+    $("div").addClass("active");   
+}
+//
+
+
+
+
+
